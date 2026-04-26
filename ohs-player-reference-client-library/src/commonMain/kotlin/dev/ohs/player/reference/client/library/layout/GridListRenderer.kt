f72657b43fa9995139b89862dc5df9ffd1fde466
fikrimilano <fikrimilano1@gmail.com>
2026-04-26 21:52:57 +0700

    Created layout renderer for grid, horizontal, and vertical list

---
+++ ohs-player-reference-client-library/src/commonMain/kotlin/dev/ohs/player/reference/client/library/layout/GridListRenderer.kt
@@ -0,0 +1,44 @@
+package dev.ohs.player.reference.client.library.layout
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import dev.ohs.player.reference.client.library.renderer.LayoutRenderer
+import dev.ohs.player.reference.client.library.renderer.Renderer
+
+class GridListRenderer<T>(
+    private val cells: GridCells = GridCells.Fixed(2),
+    private val contentPadding: PaddingValues = PaddingValues(0.dp),
+    private val itemSpacing: Dp = 0.dp,
+) : LayoutRenderer<T> {
+    @Composable
+    override fun Render(
+        items: List<T>,
+        itemRenderer: Renderer<T>,
+        key: (T) -> Any,
+        onItemClick: (T) -> Unit,
+        modifier: Modifier,
+    ) {
+        LazyVerticalGrid(
+            columns = cells,
+            modifier = modifier,
+            contentPadding = contentPadding,
+            verticalArrangement = Arrangement.spacedBy(itemSpacing),
+            horizontalArrangement = Arrangement.spacedBy(itemSpacing),
+        ) {
+            items(items = items, key = key) { item ->
+                itemRenderer.Render(
+                    item = item,
+                    onClick = { onItemClick(item) },
+                    modifier = Modifier,
+                )
+            }
+        }
+    }
+}

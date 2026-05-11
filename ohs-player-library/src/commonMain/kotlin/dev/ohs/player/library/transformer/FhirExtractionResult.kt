41c8d0a6f158a4ee0e8e86c46dc9062ed1cdab50
David <davidnjau21@gmail.com>
2026-05-11 20:33:22 +0300

    Add Fhir extraction result and repository

---
+++ ohs-player-library/src/commonMain/kotlin/dev/ohs/player/library/transformer/FhirExtractionResult.kt
@@ -0,0 +1,28 @@
+package dev.ohs.player.library.transformer
+
+import dev.ohs.fhir.model.r4.Resource
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonNull
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.decodeFromJsonElement
+
+class FhirExtractionResult(
+    val resource: Resource,
+    val values: Map<String, String?>,
+    @PublishedApi internal val json: Json = Json {
+        ignoreUnknownKeys = true
+        coerceInputValues = true
+    }
+) {
+    operator fun get(field: String): String? = values[field]
+
+    fun toJsonObject(): JsonObject = JsonObject(
+        values.mapValues { (_, v) ->
+            if (v != null) JsonPrimitive(v) else JsonNull
+        }
+    )
+
+    inline fun <reified T : Any> decode(): T =
+        json.decodeFromJsonElement(toJsonObject())
+}
---
+++ ohs-player-library/src/commonMain/kotlin/dev/ohs/player/library/transformer/FhirExtractorRepository.kt
@@ -0,0 +1,13 @@
+package dev.ohs.player.library.transformer
+
+import dev.ohs.fhir.model.r4.Resource
+import dev.ohs.player.library.model.ViewDefinition
+
+interface FhirExtractorRepository {
+    fun extract(resource: Resource, viewDefinition: ViewDefinition): FhirExtractionResult
+}
+
+class ExtractFhirDataUseCase(private val repository: FhirExtractorRepository){
+    operator fun invoke(resource: Resource, viewDefinition: ViewDefinition) : FhirExtractionResult =
+        repository.extract(resource, viewDefinition)
+}

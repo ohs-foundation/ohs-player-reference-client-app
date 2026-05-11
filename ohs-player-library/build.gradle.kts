b718ea7de69815480a45f7eb55b2ae64aab0479f
David <davidnjau21@gmail.com>
2026-05-11 20:04:29 +0300

    Update fhir model in library

--- ohs-player-library/build.gradle.kts
+++ ohs-player-library/build.gradle.kts
@@ -36,8 +36,7 @@ kotlin {
     }
 
     sourceSets {
-        commonTest.dependencies {
-            implementation(libs.kotlin.test)
+        commonMain.dependencies {
 //            api(libs.kotlinx.serialization.json)
             implementation("dev.ohs.fhir:fhir-model:1.0.0-beta03")
             api("dev.ohs.fhir:fhir-path:1.0.0-beta02")
---
+++ ohs-player-library/src/commonMain/kotlin/dev/ohs/player/library/transformer/DataTransformer.kt
@@ -0,0 +1,4 @@
+package dev.ohs.player.library.transformer
+
+class DataTransformer {
+}
